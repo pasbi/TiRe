cbcefa7dc20da929abd2ee70579e00e202f5e6c8
pascal <pascal@pakab.de>
2024-08-25 15:21:46 +0200

    fix bug in overhour calulcation

--- src/views/planview.cpp
+++ src/views/planview.cpp
@@ -50,12 +50,14 @@ void PlanView::invalidate()
   const auto& interval_model = time_sheet()->interval_model();
   const auto& plan = time_sheet()->plan();
   const auto actual_working_time = interval_model.minutes(current_period(), Project::Type::Work);
+  const auto actual_leave_time = interval_model.minutes(current_period(), Project::Type::Holiday)
+                                 + interval_model.minutes(current_period(), Project::Type::Sick);
   const auto planned_working_time = plan.planned_working_time(current_period().begin(), current_period().end());
   m_ui->lb_total->setText(::format_minutes(actual_working_time));
   m_ui->lb_holiday->setText(::format_minutes(interval_model.minutes(current_period(), Project::Type::Holiday)));
   m_ui->lb_sick->setText(::format_minutes(interval_model.minutes(current_period(), Project::Type::Sick)));
   m_ui->lb_planned->setText(::format_minutes(planned_working_time));
-  m_ui->lb_overtime->setText(::format_minutes(actual_working_time - planned_working_time));
+  m_ui->lb_overtime->setText(::format_minutes(actual_working_time - planned_working_time + actual_leave_time));
 
   const auto total_overtime = plan.overtime_offset() + interval_model.minutes({}, Project::Type::Work)
                               - plan.planned_working_time({}, QDate::currentDate());
