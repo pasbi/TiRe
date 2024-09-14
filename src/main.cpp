3f7297e74e45ad2df40609bf02dd9b4fd564189b
pascal <pascal@pakab.de>
2024-09-14 12:28:26 +0200

    use a CommandLineParser

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QCommandLineParser>
+
+namespace
+{
+
+constexpr auto timesheet_filename_option_name = "timesheet-filename";
+
+[[nodiscard]] auto command_line_args()
+{
+  auto clp = std::make_unique<QCommandLineParser>();
+  clp->addPositionalArgument(timesheet_filename_option_name, "Path to the time sheet (JSON).", "FILENAME");
+  clp->process(*QApplication::instance());
+  return clp;
+}
+
+}  // namespace
 
 int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
 
   MainWindow w;
-  if (const auto args = QApplication::arguments(); args.size() > 1) {
-    w.load(static_cast<std::filesystem::path>(args.at(1).toStdString()));
+  const auto clp = command_line_args();
+  if (const auto& args = clp->positionalArguments(); !args.empty()) {
+    w.load(static_cast<std::filesystem::path>(args.front().toStdString()));
   }
   w.show();
   QApplication::exec();
